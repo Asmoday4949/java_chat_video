4d1dba258783a95123479867485e5cc3705d5383
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-05-24 10:01:27 +0200

    use and creating packages

---
+++ PCoursJava/src/ch/hearc/cours/videochat/webcam/UseTestWebcam.java
@@ -0,0 +1,22 @@
+
+package ch.hearc.cours.videochat.webcam;
+
+
+
+public class UseTestWebcam
+	{
+	/*------------------------------------------------------------------*\
+	|*							Methodes Public							*|
+	\*------------------------------------------------------------------*/
+
+	public static void main(String[] args)
+		{
+		main();
+		}
+
+	public static void main()
+		{
+
+		}
+	}
+
